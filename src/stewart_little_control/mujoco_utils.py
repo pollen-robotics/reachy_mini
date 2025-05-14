ba42fdd6bbb33b5dda998a326a100d501b6c3fb9
RemiFabre <remifabre1800@gmail.com>
2025-05-14 22:56:48 +0200

    Added pose generation utils and lit of joints

--- src/stewart_little_control/mujoco_utils.py
+++ src/stewart_little_control/mujoco_utils.py
@@ -1,12 +1,46 @@
 import mujoco
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+def get_homogeneous_matrix_from_euler(
+    position: tuple = (0, 0, 0),  # (x, y, z)
+    euler_angles: tuple = (0, 0, 0),  # (roll, pitch, yaw)
+    degrees: bool = False,
+):
+    homogeneous_matrix = np.eye(4)
+    homogeneous_matrix[:3, :3] = R.from_euler("xyz", euler_angles, degrees=degrees).as_matrix()
+    homogeneous_matrix[:3, 3] = position
+    return homogeneous_matrix
 
 def get_joint_qpos(model, data, joint_name):
     # Get the joint id
     joint_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, joint_name)
-
+    if joint_id == -1:
+        raise ValueError(f"Joint '{joint_name}' not found.")
+    
     # Get the address of the joint's qpos in the qpos array
     qpos_addr = model.jnt_qposadr[joint_id]
-
+    
     # Get the qpos value
-    joint_qpos = data.qpos[qpos_addr]
-    return joint_qpos
+    return data.qpos[qpos_addr]
+
+def get_joints(model, data):
+    joints = []
+    for i in range(6):
+        print(i)
+        joints.append(get_joint_qpos(model, data, str(i+1)))
+    return joints
+
+
+
+def get_joints(model, data):
+    """
+    Gets qpos for joints named "1" through "6".
+    Returns a 1D NumPy array.
+    """
+    num_joints_to_get = 6
+    joint_qpos_values = np.empty(num_joints_to_get, dtype=float)
+    for i in range(num_joints_to_get):
+        joint_name = str(i + 1)
+        joint_qpos_values[i] = get_joint_qpos(model, data, joint_name)
+    return joint_qpos_values
