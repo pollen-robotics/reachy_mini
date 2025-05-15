04aa4d18c7f3eae7292db85e836e546375fb521d
apirrone <awanegenebayo@gmail.com>
2025-05-15 17:40:43 +0200

    friction

--- src/stewart_little_control/mujoco_client.py
+++ src/stewart_little_control/mujoco_client.py
@@ -30,14 +30,14 @@ if __name__ == "__main__":
     client = MujocoClient()
     while True:
         pose = np.eye(4)
-        # euler_rot = [
-        #     0,
-        #     0,
-        #     # 0.2 * np.sin(2 * np.pi * 0.5 * t),
-        #     1.0 * np.sin(2 * np.pi * 0.5 * time.time() + np.pi),
-        # ]
-        # rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
-        # pose[:3, :3] = rot_mat
-        pose[:3, 3][2] += 0.01 * np.sin(2 * np.pi * 0.5 * time.time())
+        euler_rot = [
+            0,
+            0,
+            # 0.2 * np.sin(2 * np.pi * 0.5 * t),
+            1.0 * np.sin(2 * np.pi * 0.5 * time.time() + np.pi),
+        ]
+        rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
+        pose[:3, :3] = rot_mat
+        # pose[:3, 3][2] += 0.01 * np.sin(2 * np.pi * 0.5 * time.time())
         client.send_pose(pose)
         time.sleep(0.02)
