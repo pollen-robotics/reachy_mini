b58316ac23e9201adde3fbfadd96347f0389a1a3
apirrone <awanegenebayo@gmail.com>
2025-05-27 18:05:59 +0200

    decimating meshes

--- examples/client_example.py
+++ examples/client_example.py
@@ -3,59 +3,72 @@ import time
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+
+"""
+Warning: Don't run this lol
+"""
 client = Client()
 
-while True:
+# pose = np.eye(4)
+# pose[:3, 3][2] = 0.18  # Set the height of the head
+# pose[:3, 3][0] = 0.02
+# euler_rot = np.array([0, 0.5, 0.0])
+# rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
+# pose[:3, :3] = rot_mat
+# client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+# exit()
 
-    pose = np.eye(4)
-    pose[:3, 3][2] = 0.177  # Set the height of the head
+# while True:
 
-    euler_rot = [
-        0,
-        0,
-        1.0 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
-    ]
-    rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
-    pose[:3, :3] = rot_mat
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
-    time.sleep(0.02)
+#     pose = np.eye(4)
+#     pose[:3, 3][2] = 0.177  # Set the height of the head
 
-# boop
-pose = np.eye(4)
-pose[:3, 3][2] = 0.177  # Set the height of the head
+#     euler_rot = [
+#         0,
+#         0,
+#         1.0 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
+#     ]
+#     rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
+#     pose[:3, :3] = rot_mat
+#     client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+#     time.sleep(0.02)
+
+# # boop
+# pose = np.eye(4)
+# pose[:3, 3][2] = 0.177  # Set the height of the head
+# # pose[:3, 3][0] = 0.01  # Set the height of the head
+# # euler_rot = np.array([0.4, 0.0, 0.0])
+
+# antennas=[0, 0]
+# client.send_pose(pose, antennas=antennas, offset_zero=False)
+# time.sleep(0.5)
+
+# # antennas=[2.7, 0]
 # pose[:3, 3][0] = 0.01  # Set the height of the head
 # euler_rot = np.array([0.4, 0.0, 0.0])
+# rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
+# pose[:3, :3] = rot_mat
+# client.send_pose(pose, antennas=antennas, offset_zero=False)
 
-antennas=[0, 0]
-client.send_pose(pose, antennas=antennas, offset_zero=False)
-time.sleep(0.5)
+# time.sleep(1.0)
+# antennas=[2.8, 0]
+# client.send_pose(pose, antennas=antennas, offset_zero=False)
 
-# antennas=[2.7, 0]
-pose[:3, 3][0] = 0.01  # Set the height of the head
-euler_rot = np.array([0.4, 0.0, 0.0])
-rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
-pose[:3, :3] = rot_mat
-client.send_pose(pose, antennas=antennas, offset_zero=False)
+# time.sleep(0.3)
+# antennas=[0, 0]
+# client.send_pose(pose, antennas=antennas, offset_zero=False)
+# time.sleep(1.0)
 
-time.sleep(1.0)
-antennas=[2.8, 0]
-client.send_pose(pose, antennas=antennas, offset_zero=False)
-
-time.sleep(0.3)
-antennas=[0, 0]
-client.send_pose(pose, antennas=antennas, offset_zero=False)
-time.sleep(1.0)
-
-euler_rot = np.array([0.0, 0.0, 0.0])
-pose[:3, 3][0] = 0.0  # Set the height of the head
-rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
-pose[:3, :3] = rot_mat
-client.send_pose(pose, antennas=antennas, offset_zero=False)
-exit()
+# euler_rot = np.array([0.0, 0.0, 0.0])
+# pose[:3, 3][0] = 0.0  # Set the height of the head
+# rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
+# pose[:3, :3] = rot_mat
+# client.send_pose(pose, antennas=antennas, offset_zero=False)
+# exit()
 
 
 
-time.sleep(1)
+# time.sleep(1)
 while True:
     # sleep_pose = np.array(
     #     [
@@ -70,15 +83,15 @@ while True:
     # continue
     pose = np.eye(4)
     pose[:3, 3][2] = 0.177  # Set the height of the head
-    pose[:3, 3][0] = 0.01  # Set the height of the head
-    euler_rot = np.array([0.4, 0.0, 0.0])
-    # euler_rot = [
-    #     # 0.3 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
-    #     0,
-    #     0,
-    #     # 0
-    #     1.0 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
-    # ]
+    # pose[:3, 3][0] = 0.01  # Set the height of the head
+    # euler_rot = np.array([0.4, 0.0, 0.0])
+    euler_rot = [
+        # 0.3 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
+        0,
+        0,
+        # 0
+        1.0 * np.sin(2 * np.pi * 0.3 * time.time() + np.pi),
+    ]
     rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
     pose[:3, :3] = rot_mat
     # pose[:3, 3][2] += 0.01 * np.sin(2 * np.pi * 1 * time.time())
