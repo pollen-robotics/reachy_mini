4aed77262935f72f0913652bd3029265273f8401
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-06-05 14:48:10 +0200

    Update test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,24 +5,33 @@ from test_cmd import assert_command_equal, random_command
 
 
 def test_client_server_io():
-    s = Server()
-    s.start()
+    try:
+        s = Server()
+        s.start()
 
-    cmd = s.get_latest_command()
-    assert_command_equal(cmd, ReachyMiniCommand().default())
+        cmd = s.get_latest_command()
+        assert_command_equal(cmd, ReachyMiniCommand().default())
 
-    c = Client()
+        c = Client()
 
-    new_cmd = random_command()
+        new_cmd = random_command()
 
-    s.command_received_event().clear()
-    c.send_command(new_cmd)
-    s.command_received_event().wait()
+        s.command_received_event().clear()
+        c.send_command(new_cmd)
+        assert s.command_received_event().wait(timeout=0.1)
 
-    recv_cmd = s.get_latest_command()
-    assert_command_equal(recv_cmd, new_cmd)
+        recv_cmd = s.get_latest_command()
+        assert_command_equal(recv_cmd, new_cmd)
 
-    recv_cmd = s.get_latest_command()
-    assert_command_equal(recv_cmd, new_cmd)
+        recv_cmd = s.get_latest_command()
+        assert_command_equal(recv_cmd, new_cmd)
 
-    s.stop()
+        s.stop()
+    except Exception as e:
+        s.stop()
+        raise e
+
+
+if __name__ == "__main__":
+    test_client_server_io()
+    print("Test passed successfully.")
