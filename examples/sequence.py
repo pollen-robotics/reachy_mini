68cee4e1de29a53f6535054dab00665ba4e3ed12
apirrone <awanegenebayo@gmail.com>
2025-06-06 14:34:53 +0200

    update sequence.py

--- examples/sequence.py
+++ examples/sequence.py
@@ -1,4 +1,5 @@
-from reachy_mini import Client
+from reachy_mini.io import Client
+from reachy_mini.command import ReachyMiniCommand
 import time
 import numpy as np
 from scipy.spatial.transform import Rotation as R
@@ -21,7 +22,14 @@ while True:
         euler_rot = np.array([0, 0.0, 0.7*np.sin(2*np.pi*0.5*t)])
         rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
         pose[:3, :3] = rot_mat
-        client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=[0, 0],
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
     s = time.time()
@@ -30,7 +38,13 @@ while True:
         euler_rot = np.array([0, 0.3*np.sin(2*np.pi*0.5*t), 0])
         rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
         pose[:3, :3] = rot_mat
-        client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=[0, 0],
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
 
@@ -40,7 +54,13 @@ while True:
         euler_rot = np.array([0.3*np.sin(2*np.pi*0.5*t), 0, 0])
         rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
         pose[:3, :3] = rot_mat
-        client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=[0, 0],
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
     s = time.time()
@@ -52,14 +72,26 @@ while True:
         # euler_rot = np.array([0.3*np.sin(2*np.pi*0.5*t), 0, 0])
         # rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
         # pose[:3, :3] = rot_mat
-        client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=[0, 0],
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
     s = time.time()
     while time.time() - s < 2.0:
         t = time.time() - t0
         antennas = [0.5*np.sin(2*np.pi*0.5*t), -0.5*np.sin(2*np.pi*0.5*t)]
-        client.send_pose(pose, antennas=antennas, offset_zero=False)
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=antennas,
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
     s = time.time()
@@ -73,29 +105,65 @@ while True:
             0.015 * np.sin(2 * np.pi * 1.0 * t + np.pi / 2),
             0.177,
         ]
-        client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+        client.send_command(
+            ReachyMiniCommand(
+                head_pose=pose,
+                antennas_orientation=[0, 0],
+                offset_zero=False,
+            )
+        )
         time.sleep(0.01)
 
     pose[:3, 3] = [0, 0, 0.177]
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+    client.send_command(
+        ReachyMiniCommand(
+            head_pose=pose,
+            antennas_orientation=[0, 0],
+            offset_zero=False,
+        )
+    )
     time.sleep(0.5)
     pose[:3, 3] = [0.02, 0.02, 0.177]
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+    client.send_command(
+        ReachyMiniCommand(
+            head_pose=pose,
+            antennas_orientation=[0, 0],
+            offset_zero=False,
+        )
+    )
     time.sleep(0.5)
     pose[:3, 3] = [0.00, 0.02, 0.177]
     euler_rot = np.array([0, 0, 0.5])
     rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
     pose[:3, :3] = rot_mat
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+    client.send_command(
+        ReachyMiniCommand(
+            head_pose=pose,
+            antennas_orientation=[0, 0],
+            offset_zero=False,
+        )
+    )
     time.sleep(0.5)
     pose[:3, 3] = [0.00, -0.02, 0.177]
     euler_rot = np.array([0, 0, -0.5])
     rot_mat = R.from_euler("xyz", euler_rot, degrees=False).as_matrix()
     pose[:3, :3] = rot_mat
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+    client.send_command(
+        ReachyMiniCommand(
+            head_pose=pose,
+            antennas_orientation=[0, 0],
+            offset_zero=False,
+        )
+    )
     time.sleep(0.5)
     pose[:3, 3] = [0, 0, 0.177]
-    client.send_pose(pose, antennas=[0, 0], offset_zero=False)
+    client.send_command(
+        ReachyMiniCommand(
+            head_pose=pose,
+            antennas_orientation=[0, 0],
+            offset_zero=False,
+        )
+    )
 
     time.sleep(2)
 
