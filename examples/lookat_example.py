c0e7ded4a53343233f12436ab164d743a05b6b53
apirrone <awanegenebayo@gmail.com>
2025-06-25 13:29:22 +0200

    im_look_at() and look_at()

---
+++ examples/lookat_example.py
@@ -0,0 +1,34 @@
+from reachy_mini import ReachyMini
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(4)
+
+
+def click(event, x, y, flags, param):
+    global click_x, click_y, just_clicked
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        just_clicked = True
+        click_x, click_y = x, y
+
+
+click_x, click_y = 0, 0
+just_clicked = False
+frame = np.zeros((1280, 720, 3))
+cv2.namedWindow("Frame")
+cv2.setMouseCallback("Frame", click)
+
+
+with ReachyMini() as reachy_mini:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
+
+        if just_clicked:
+            reachy_mini.im_look_at(click_x, click_y, duration=0.3)
+            just_clicked = False
