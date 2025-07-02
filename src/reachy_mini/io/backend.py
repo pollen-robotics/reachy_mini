e5ed611d325fcf33391ecb238be96bc12f929bdc
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-07-02 11:51:07 +0200

    Improve error logs.

--- src/reachy_mini/io/backend.py
+++ src/reachy_mini/io/backend.py
@@ -16,14 +16,16 @@ class Backend:
         self.joint_positions_publisher = None  # Placeholder for a publisher object
 
     def run(self):
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError("The method run should be overridden by subclasses.")
 
     def close(self) -> None:
         """
         Close the backend.
         This method is a placeholder and should be overridden by subclasses.
         """
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError(
+            "The method close should be overridden by subclasses."
+        )
 
     def set_joint_positions_publisher(self, publisher) -> None:
         self.joint_positions_publisher = publisher
@@ -35,17 +37,25 @@ class Backend:
         self.antenna_joint_positions = positions
 
     def set_torque(self, enabled: bool) -> None:
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError(
+            "The method set_torque should be overridden by subclasses."
+        )
 
     def get_head_joint_positions(self) -> List[float]:
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError(
+            "The method get_head_joint_positions should be overridden by subclasses."
+        )
 
     def get_antenna_joint_positions(self) -> List[float]:
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError(
+            "The method get_antenna_joint_positions should be overridden by subclasses."
+        )
 
     def get_status(self):
         """
         Returns backend statistics.
         This method is a placeholder and should be overridden by subclasses.
         """
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        raise NotImplementedError(
+            "The method get_status should be overridden by subclasses."
+        )
