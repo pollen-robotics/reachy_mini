4424514045100fca5ce0776c7f52e3e89e21954c
gospar <antun.skuric@outlook.com>
2025-07-11 09:48:56 +0200

    isort for __init__.py

--- src/reachy_mini/__init__.py
+++ src/reachy_mini/__init__.py
@@ -1,5 +1,6 @@
 """Reachy Mini SDK."""
 
+from reachy_mini.analytic_kinematics import \
+    ReachyMiniAnalyticKinematics  # noqa: F401
 from reachy_mini.app import ReachyMiniApp  # noqa: F401
 from reachy_mini.reachy_mini import ReachyMini  # noqa: F401
-from reachy_mini.analytic_kinematics import ReachyMiniAnalyticKinematics  # noqa: F401
