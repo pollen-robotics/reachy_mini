2f3b83ca4c6c653474d8f42768ac4372c0a6c666
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-07-15 14:33:33 +0200

    Typo.

--- examples/reachy_compliant_demo.py
+++ examples/reachy_compliant_demo.py
@@ -1,4 +1,11 @@
-"""Reachy Mini Compliant Demo."""
+"""Reachy Mini Compliant Demo.
+
+First, this demo turns the Reachy Mini into compliant mode.
+Then, it compensates for the gravity of the robot platform to prevent it from falling down.
+
+You can now gently push the robot and it will follow your movements. And when you stop pushing it, it will stay in place.
+This is useful for applications like human-robot interaction, where you want the robot to be compliant and follow the user's movements.
+"""
 
 import time
 
