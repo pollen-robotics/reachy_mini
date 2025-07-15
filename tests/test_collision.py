b2860323f1af8d0818ddf8c0f9198b49d3ca05b7
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-07-15 14:37:59 +0200

    Update test_collision.py

--- tests/test_collision.py
+++ tests/test_collision.py
@@ -1,13 +1,17 @@
 from reachy_mini import ReachyMini
+from reachy_mini.placo_kinematics import PlacoKinematics
 from reachy_mini.utils import create_head_pose
 
 
-def test_collision():
-    with ReachyMini() as mini:
-        reachable_pose = create_head_pose()
-        sol = mini.head_kinematics.ik(reachable_pose, check_collision=True)
-        assert sol is not None, "The reachable pose should not cause a collision."
+def offline_test_collision():
+    head_kinematics = PlacoKinematics(
+        urdf_path=ReachyMini.urdf_root_path
+    )
 
-        unreachable_pose = create_head_pose(x=20, y=20, mm=True)
-        sol = mini.head_kinematics.ik(unreachable_pose, check_collision=True)
-        assert sol is None, "The unreachable pose should cause a collision."
+    reachable_pose = create_head_pose()
+    sol = head_kinematics.ik(reachable_pose, check_collision=True)
+    assert sol is not None, "The reachable pose should not cause a collision."
+
+    unreachable_pose = create_head_pose(x=20, y=20, mm=True)
+    sol = head_kinematics.ik(unreachable_pose, check_collision=True)
+    assert sol is None, "The unreachable pose should cause a collision."
