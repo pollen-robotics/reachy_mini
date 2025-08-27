96666acb9c6bbf31cf53e69cd793728c3c2117f0
apirrone <awanegenebayo@gmail.com>
2025-08-27 15:29:33 +0200

    compare placo nn kin

--- examples/debug/compare_placo_nn_kin.py
+++ examples/debug/compare_placo_nn_kin.py
@@ -1,8 +1,10 @@
-from reachy_mini.placo_kinematics import PlacoKinematics
-from reachy_mini.nn_kinematics import NNKinematics
-import os
+import os  # noqa: D100
+
 import numpy as np
 from placo_utils.tf import tf
+
+from reachy_mini.nn_kinematics import NNKinematics
+from reachy_mini.placo_kinematics import PlacoKinematics
 
 urdf_path = os.path.abspath(
     "../../src/reachy_mini/descriptions/reachy_mini/urdf/robot.urdf"
@@ -18,7 +20,7 @@ while i < 2000:
     px, py, pz = [np.random.uniform(-0.01, 0.01) for _ in range(3)]
     roll, pitch = [np.random.uniform(-np.deg2rad(30), np.deg2rad(30)) for _ in range(2)]
     yaw = np.random.uniform(-2.8, 2.8)
-    yaw = 0
+    # yaw = 0
     body_yaw = -yaw  # + np.random.uniform(-np.deg2rad(20), np.deg2rad(20))
     body_yaw = 0
 
@@ -26,7 +28,9 @@ while i < 2000:
         roll, pitch, yaw
     )
 
-    placo_result = placo_kinematics.ik(pose=T_head_target, body_yaw=body_yaw, no_iterations=20)
+    placo_result = placo_kinematics.ik(
+        pose=T_head_target, body_yaw=body_yaw, no_iterations=20
+    )
     nn_result = nn_kinematics.ik(pose=T_head_target, body_yaw=body_yaw)
 
     print(f"Placo Kinematics Result: {np.around(placo_result, 3)}")
