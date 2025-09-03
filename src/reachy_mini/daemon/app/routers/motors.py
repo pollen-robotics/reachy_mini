effc898bbcda2e1b8839e2c652670f45fea5ee04
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-09-03 12:49:28 +0200

    Add support for gravity compensation.

--- src/reachy_mini/daemon/app/routers/motors.py
+++ src/reachy_mini/daemon/app/routers/motors.py
@@ -1,27 +1,28 @@
-from enum import Enum
-
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 
-from ....daemon.backend.abstract import Backend
+from ....daemon.backend.abstract import Backend, MotorControlMode
 from ..dependencies import get_backend
-
-
-class MotorMode(str, Enum):
-    Enabled = "enabled"
-    Disabled = "disabled"
-
 
 router = APIRouter(
     prefix="/motors",
 )
 
 
+class MotorStatus(BaseModel):
+    mode: MotorControlMode
+
+
+@router.get("/status")
+async def get_motor_status(backend: Backend = Depends(get_backend)) -> MotorStatus:
+    return MotorStatus(mode=backend.get_motor_control_mode())
+
+
 @router.post("/set_mode/{mode}")
-async def set_motor_mode(mode: MotorMode, backend: Backend = Depends(get_backend)):
-    if mode == MotorMode.Enabled:
-        backend.enable_motors()
-    elif mode == MotorMode.Disabled:
-        backend.disable_motors()
-    else:
-        raise ValueError(f"Unknown motor mode: {mode}")
+async def set_motor_mode(
+    mode: MotorControlMode,
+    backend: Backend = Depends(get_backend),
+):
+    backend.set_motor_control_mode(mode)
+
     return {"status": f"motors changed to {mode} mode"}
