644ae1a18789ce93708c744dbb21103f6443f79c
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-09-03 16:43:19 +0200

    Update dependencies.py

--- src/reachy_mini/daemon/app/dependencies.py
+++ src/reachy_mini/daemon/app/dependencies.py
@@ -1,3 +1,5 @@
+"""FastAPI common request dependencies."""
+
 from fastapi import Request, WebSocket
 
 from ..backend.abstract import Backend
@@ -5,12 +7,15 @@ from ..daemon import Daemon
 
 
 def get_daemon(request: Request) -> Daemon:
+    """Get the daemon as request dependency."""
     return request.app.state.daemon
 
 
 def get_backend(request: Request) -> Backend:
+    """Get the backend as request dependency."""
     return request.app.state.daemon.backend
 
 
 def ws_get_backend(websocket: WebSocket) -> Backend:
+    """Get the backend as websocket dependency."""
     return websocket.app.state.daemon.backend
