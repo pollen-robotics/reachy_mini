59edebee4afd96f621afacbd12f71ee25c71eb71
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-09-05 12:11:32 +0200

    Add target in state.

--- src/reachy_mini/daemon/app/routers/state.py
+++ src/reachy_mini/daemon/app/routers/state.py
@@ -53,12 +53,17 @@ async def get_antenna_joint_positions(
     return (pos[0], pos[1])
 
 
+# TODO: get target back
 @router.get("/full")
 async def get_full_state(
     with_head_pose: bool = True,
+    with_target_head_pose: bool = False,
     with_head_joints: bool = True,
+    with_target_head_joints: bool = False,
     with_body_yaw: bool = True,
+    with_target_body_yaw: bool = False,
     with_antenna_positions: bool = True,
+    with_target_antenna_positions: bool = False,
     use_pose_matrix: bool = False,
     backend: Backend = Depends(get_backend),
 ) -> FullState:
@@ -68,12 +73,21 @@ async def get_full_state(
     if with_head_pose:
         pose = backend.get_present_head_pose()
         result["head_pose"] = as_any_pose(pose, use_pose_matrix)
+    if with_target_head_pose:
+        target_pose = backend.target_head_pose
+        result["target_head_pose"] = as_any_pose(target_pose, use_pose_matrix)
     if with_head_joints:
         result["head_joints"] = backend.get_present_head_joint_positions()
+    if with_target_head_joints:
+        result["target_head_joints"] = backend.target_head_joint_positions
     if with_body_yaw:
         result["body_yaw"] = backend.get_present_body_yaw()
+    if with_target_body_yaw:
+        result["target_body_yaw"] = backend.target_body_yaw
     if with_antenna_positions:
         result["antennas_position"] = backend.get_present_antenna_joint_positions()
+    if with_target_antenna_positions:
+        result["target_antennas_position"] = backend.target_antenna_joint_positions
 
     result["timestamp"] = datetime.now(timezone.utc)
     return FullState.model_validate(result)
@@ -84,9 +98,13 @@ async def ws_full_state(
     websocket: WebSocket,
     frequency: float = 10.0,
     with_head_pose: bool = True,
+    with_target_head_pose: bool = False,
     with_head_joints: bool = True,
+    with_target_head_joints: bool = False,
     with_body_yaw: bool = True,
+    with_target_body_yaw: bool = False,
     with_antenna_positions: bool = True,
+    with_target_antenna_positions: bool = False,
     use_pose_matrix: bool = False,
     backend: Backend = Depends(ws_get_backend),
 ):
@@ -98,9 +116,13 @@ async def ws_full_state(
         while True:
             full_state = await get_full_state(
                 with_head_pose=with_head_pose,
+                with_target_head_pose=with_target_head_pose,
                 with_head_joints=with_head_joints,
+                with_target_head_joints=with_target_head_joints,
                 with_body_yaw=with_body_yaw,
+                with_target_body_yaw=with_target_body_yaw,
                 with_antenna_positions=with_antenna_positions,
+                with_target_antenna_positions=with_target_antenna_positions,
                 use_pose_matrix=use_pose_matrix,
                 backend=backend,
             )
