579363703b3a7934a8966961f825d7b30d1f1aa6
haixuantao <tao.xavier@outlook.com>
2025-09-05 16:28:00 +0200

    Add daemon test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,12 @@
 def test_import():  # noqa: D100, D103
     from reachy_mini import ReachyMini  # noqa: F401
     from reachy_mini.app import ReachyMiniApp  # noqa: F401
-    from reachy_mini.daemon.cli import main  # noqa: F401
+    
+def test_daemon():  # noqa: D100, D103
+    from reachy_mini.daemon.daemon import Daemon    
+    
+    daemon = Daemon()
+    daemon.start(
+        sim=True
+    )
+    daemon.stop()
