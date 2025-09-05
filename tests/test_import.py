be50ecc3b53257c4ae633c8c31bd2181e36c8978
haixuantao <tao.xavier@outlook.com>
2025-09-05 16:25:40 +0200

    Add daemon main import within pytest

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,4 @@
 def test_import():  # noqa: D100, D103
     from reachy_mini import ReachyMini  # noqa: F401
     from reachy_mini.app import ReachyMiniApp  # noqa: F401
+    from reachy_mini.daemon.cli import main  # noqa: F401
