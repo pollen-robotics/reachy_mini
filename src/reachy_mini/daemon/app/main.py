5b377bb3d5bab10a87249a14da5041564429e6c5
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-09-11 10:35:46 +0200

    Add args in main entry point.

--- src/reachy_mini/daemon/app/main.py
+++ src/reachy_mini/daemon/app/main.py
@@ -1,10 +1,19 @@
-"""Main API entry point server."""
+"""Daemon entry point for the Reachy Mini robot.
 
+This script serves as the command-line interface (CLI) entry point for the Reachy Mini daemon.
+It initializes the daemon with specified parameters such as simulation mode, serial port,
+scene to load, and logging level. The daemon runs indefinitely, handling requests and
+managing the robot's state.
+
+"""
+
+import argparse
 import logging
 import os
 from contextlib import asynccontextmanager
 from pathlib import Path
 
+import uvicorn
 from fastapi import APIRouter, FastAPI, HTTPException, Request, responses
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
@@ -12,10 +21,6 @@ from fastapi.templating import Jinja2Templates
 from reachy_mini.apps.manager import AppManager
 from reachy_mini.daemon.app.routers import apps, daemon, kinematics, motors, move, state
 from reachy_mini.daemon.daemon import Daemon
-
-# from ...apps.manager import AppManager
-# from ..daemon import Daemon
-# from .routers import apps, daemon, kinematics, motors, move, state
 
 EXAMPLES_WEB_DIR = Path(__file__).parent / "examples"
 TEMPLATES_DIR = Path(__file__).parent / "templates"
@@ -26,14 +31,14 @@ templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 async def lifespan(app: FastAPI):
     """Lifespan context manager for the FastAPI application."""
     await app.state.daemon.start(
-        sim=app.state.params["sim"],
-        scene=app.state.params["scene"],
-        wake_up_on_start=app.state.params["wake_up_on_start"],
+        sim=app.state.args.sim,
+        scene=app.state.args.scene,
+        wake_up_on_start=app.state.args.wake_up_on_start,
     )
     yield
     await app.state.app_manager.close()
     await app.state.daemon.stop(
-        goto_sleep_on_stop=app.state.params["goto_sleep_on_stop"]
+        goto_sleep_on_stop=app.state.args.goto_sleep_on_stop,
     )
 
 
@@ -44,12 +49,7 @@ app = FastAPI(
 )
 app.state.daemon = Daemon()
 app.state.app_manager = AppManager()
-app.state.params = {
-    "sim": False,
-    "scene": "empty",
-    "wake_up_on_start": False,
-    "goto_sleep_on_stop": False,
-}
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -90,7 +90,95 @@ async def serve_example(filename: str):
     return responses.FileResponse(file_path)
 
 
-if __name__ == "__main__":
-    import uvicorn
+def main():
+    """Run the FastAPI app with Uvicorn."""
+    parser = argparse.ArgumentParser(description="Run the Reachy Mini daemon.")
+    # Real robot mode
+    parser.add_argument(
+        "-p",
+        "--serialport",
+        type=str,
+        default="auto",
+        help="Serial port for real motors (default: will try to automatically find the port).",
+    )
+    # Simulation mode
+    parser.add_argument(
+        "--sim",
+        action="store_true",
+        help="Run in simulation mode using Mujoco.",
+    )
+    parser.add_argument(
+        "--scene",
+        type=str,
+        default="empty",
+        help="Name of the scene to load (default: empty)",
+    )
+    # Daemon options
+    parser.add_argument(
+        "--wake-up-on-start",
+        action="store_true",
+        default=True,
+        help="Wake up the robot on daemon start (default: True).",
+    )
+    parser.add_argument(
+        "--goto-sleep-on-stop",
+        action="store_true",
+        default=True,
+        help="Put the robot to sleep on daemon stop (default: True).",
+    )
+    # Zenoh server options
+    parser.add_argument(
+        "--localhost-only",
+        action="store_true",
+        default=True,
+        help="Restrict the server to localhost only (default: True).",
+    )
+    parser.add_argument(
+        "--no-localhost-only",
+        action="store_false",
+        dest="localhost_only",
+        help="Allow the server to listen on all interfaces (default: False).",
+    )
+    # Kinematics options
+    parser.add_argument(
+        "--check-collision",
+        action="store_true",
+        default=False,
+        help="Enable collision checking (default: False).",
+    )
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    parser.add_argument(
+        "--kinematics-engine",
+        type=str,
+        default="Placo",
+        choices=["Placo", "NN", "Analytical"],
+        help="Set the kinematics engine (default: Placo).",
+    )
+    # FastAPI server options
+    parser.add_argument(
+        "--fastapi-host",
+        type=str,
+        default="0.0.0.0",
+    )
+    parser.add_argument(
+        "--fastapi-port",
+        type=int,
+        default=8000,
+    )
+    # Logging options
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level (default: INFO).",
+    )
+
+    args = parser.parse_args()
+
+    app.state.args = args
+    uvicorn.run(app, host=args.fastapi_host, port=args.fastapi_port)
+
+
+if __name__ == "__main__":
+    main()
