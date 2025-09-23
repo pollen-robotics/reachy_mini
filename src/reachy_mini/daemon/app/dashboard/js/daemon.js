80495f95534f818fd9942f0f2bd87b9416b4249f
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-09-23 13:59:55 +0200

    Improve dashboard.

--- src/reachy_mini/daemon/app/dashboard/js/daemon.js
+++ src/reachy_mini/daemon/app/dashboard/js/daemon.js
@@ -33,7 +33,7 @@ const getDaemonStatus = async () => {
 
 const checkDaemonStatus = async () => {
     const status = await getDaemonStatus();
-    applyDaemonStatus(status.state);
+    applyDaemonStatus(status);
 };
 
 const onDaemonToggleSwitch = () => {
@@ -61,7 +61,7 @@ const onDaemonToggleSwitch = () => {
     overlay.classList.add('active');
 
     if (isOn) {
-        applyDaemonStatus('starting');
+        applyDaemonStatus({ state: 'starting' });
 
         fetch(`/api/daemon/start?wake_up=${wakeUpOnStart}`, { method: 'POST' })
             .then(response => {
@@ -75,6 +75,7 @@ const onDaemonToggleSwitch = () => {
             })
             .catch(error => {
                 toggle.checked = false; // Revert toggle on error
+                checkDaemonStatus();
             })
             .finally(() => {
                 toggle.disabled = false;
@@ -82,7 +83,7 @@ const onDaemonToggleSwitch = () => {
                 overlay.classList.remove('active');
             });
     } else {
-        applyDaemonStatus('stopping');
+        applyDaemonStatus({ state: 'stopping' });
 
         fetch(`/api/daemon/stop?goto_sleep=${gotoSleepOnStop}`, { method: 'POST' })
             .then(response => {
@@ -96,6 +97,7 @@ const onDaemonToggleSwitch = () => {
             })
             .catch(error => {
                 toggle.checked = true; // Revert toggle on error
+                checkDaemonStatus();
             })
             .finally(() => {
                 toggle.disabled = false;
@@ -105,54 +107,88 @@ const onDaemonToggleSwitch = () => {
     }
 };
 
-let currentDaemonState;
-const applyDaemonStatus = (state) => {
-    if (state === currentDaemonState) return;
-    currentDaemonState = state;
 
-    console.log('Applying daemon state:', state);
+
+let currentDaemonState;
+const applyDaemonStatus = (status) => {
+    // State update
+    if (status.state === currentDaemonState) return;
+    currentDaemonState = status.state;
 
     const overlay = document.getElementById('daemon-loading-overlay');
-    overlay.classList.remove('active');
-
     const toggle = document.getElementById('daemon-toggle');
-
+    const toggleOnLabel = document.getElementById('daemon-toggle-on');
+    const toggleOffLabel = document.getElementById('daemon-toggle-off');
     const thumb = document.getElementById('daemon-mini-thumb')
-    thumb.classList = 'rounded-sm'; // Reset classes
-
-    thumb.innerHTML = ''; // Clear previous content
     const thumbImgObj = document.createElement('object');
+    const backendStatus = document.getElementById('backend-status');
+    const backendStatusIcon = document.getElementById('backend-status-icon');
+    const backendStatusText = document.getElementById('backend-status-text');
+    const collapseToggle = document.getElementById('collapse-toggle');
+    const collapseContent = document.getElementById('collapse-content');
+
+    overlay.classList.remove('active');
+    thumb.classList = 'rounded-sm'; // Reset classes
+    thumb.innerHTML = ''; // Clear previous content
     thumbImgObj.type = 'image/svg+xml';
     thumbImgObj.classList = 'w-full';
     thumb.appendChild(thumbImgObj);
+
+    backendStatus.classList = ''; // Reset classes
+    backendStatusIcon.classList = 'inline-block  w-4 h-4 rounded-full align-middle mr-2';
+    collapseToggle.classList.remove('hidden');
+    collapseContent.innerHTML = ''; // Clear previous content
 
     if (currentDaemonState === 'starting') {
         toggle.checked = true;
         thumb.classList.add('starting');
         overlay.classList.add('active');
         thumbImgObj.data = '/dashboard/assets/reachy-mini-wake-up-animation.svg';
+        backendStatusIcon.classList.add('bg-yellow-500');
+        backendStatusText.textContent = 'Waking up...';
+        collapseToggle.classList.add('hidden');
     }
     else if (currentDaemonState === 'running') {
         toggle.checked = true;
         thumb.classList.add('running');
         thumbImgObj.data = '/dashboard/assets/reachy-mini-awake.svg';
+        backendStatusIcon.classList.add('bg-green-500');
+        backendStatusText.textContent = 'Up and ready';
+        collapseToggle.classList.add('hidden');
     }
     else if (currentDaemonState === 'stopping') {
         toggle.checked = false;
         thumb.classList.add('stopping');
         overlay.classList.add('active');
         thumbImgObj.data = '/dashboard/assets/reachy-mini-go-to-sleep-animation.svg';
+        backendStatusIcon.classList.add('bg-yellow-500');
+        backendStatusText.textContent = 'Going to sleep...';
+        collapseToggle.classList.add('hidden');
     }
-    else if (currentDaemonState === 'stopped') {
+    else if (currentDaemonState === 'stopped' || currentDaemonState === 'not_initialized') {
         toggle.checked = false;
         thumb.classList.add('stopped');
         thumbImgObj.data = '/dashboard/assets/reachy-mini-sleeping.svg';
+        backendStatus.classList.add('hidden');
     } else if (currentDaemonState === 'error') {
         toggle.checked = false;
         thumb.classList.add('error');
         thumbImgObj.data = '/dashboard/assets/reachy-mini-ko-animation.svg';
+        backendStatusIcon.classList.add('bg-red-500');
+        backendStatusText.textContent = 'ERROR';
+        collapseContent.innerHTML = status.error;
+
+
     } else {
         console.error('Unknown daemon state:', currentDaemonState);
         return;
+    }
+
+    if (toggle.checked) {
+        toggleOnLabel.classList.remove('hidden');
+        toggleOffLabel.classList.add('hidden');
+    } else {
+        toggleOnLabel.classList.add('hidden');
+        toggleOffLabel.classList.remove('hidden');
     }
 };
