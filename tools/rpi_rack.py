313a5274077319c209b04f322ca6aae27b365587
apirrone <awanegenebayo@gmail.com>
2025-10-13 14:07:23 +0200

    rpi_rack.py

--- tools/rpi_rack.py
+++ tools/rpi_rack.py
@@ -23,10 +23,10 @@ def get_channel_binary(channel) -> List[int]:
 def select_channel(channel: int):
     """Select a channel on the multiplexer."""
     bits = get_channel_binary(channel)
-    S0.value = bits[0]
-    S1.value = bits[1]
-    S2.value = bits[2]
-    S3.value = bits[3]
+    S0.on() if bits[0] else S0.off()
+    S1.on() if bits[1] else S1.off()
+    S2.on() if bits[2] else S2.off()
+    S3.on() if bits[3] else S3.off()
 
 
 def lookup_for_motor(serial_port: str, id: int, baudrate: int) -> bool:
@@ -63,8 +63,10 @@ if __name__ == "__main__":
 
     channel = id_to_channel.get(args.id, None)
 
-    select_channel(channel)
-    print(f"Selected channel {channel}")
+    select_channel(0)
+    print(f"Selected channel {0}")
+    ret = lookup_for_motor(args.serial, 30, baudrate=10000000)
+    exit()
     baudrates = [57600, 1_000_000]
     ok = False
     for baudrate in baudrates:
