ffec595042d223b5bdacdf97ee0eca9d3ec47cd0
cdussieux <clementdussieux@hotmail.fr>
2025-10-13 16:56:25 +0200

    Update launcher.sh

--- services/wireless/launcher.sh
+++ services/wireless/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/pi/venv/reachymini/bin/activate
+source /home/pollen/venvs/mini_daemon/bin/activate
 python -m reachy_mini.daemon.app.main --wireless-version --no-autostart
