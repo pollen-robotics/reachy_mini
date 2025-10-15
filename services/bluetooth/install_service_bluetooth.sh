ace504a19c6ac17aa77a35fe57cb0f17bd8ac742
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2025-10-15 15:47:06 +0200

    more robust bluetooth failsafe setup

--- services/bluetooth/install_service_bluetooth.sh
+++ services/bluetooth/install_service_bluetooth.sh
@@ -3,9 +3,13 @@
 SERVICE_NAME="reachy-mini-bluetooth"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
 LAUNCHER_PATH="$(pwd)/bluetooth_service.py"
+COMMANDS_DIR="$(pwd)/commands"
+SERVICE_PATH=/bluetooth/bluetooth_service.py
 
+sudo cp "$LAUNCHER_PATH" "$SERVICE_PATH"
+sudo cp -r "$COMMANDS_DIR" /bluetooth/commands
 # Ensure Python script is executable
-chmod +x "$LAUNCHER_PATH"
+sudo chmod +x "$SERVICE_PATH"
 
 # Create the systemd service file
 cat <<EOF | sudo tee "$SERVICE_FILE" > /dev/null
@@ -16,10 +20,10 @@ Requires=bluetooth.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 $LAUNCHER_PATH
+ExecStart=/usr/bin/python3 $SERVICE_PATH
 Restart=on-failure
 User=$(whoami)
-WorkingDirectory=$(dirname "$LAUNCHER_PATH")
+WorkingDirectory=$(dirname "$SERVICE_PATH")
 
 [Install]
 WantedBy=multi-user.target
