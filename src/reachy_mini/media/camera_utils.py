8fa0ec59429e58b2b99ab402c46f091a40e4e798
apirrone <awanegenebayo@gmail.com>
2025-10-30 09:44:55 +0100

    looking for raspicam first

--- src/reachy_mini/media/camera_utils.py
+++ src/reachy_mini/media/camera_utils.py
@@ -5,10 +5,36 @@ import platform
 import cv2
 from cv2_enumerate_cameras import enumerate_cameras
 
+RPICAM = (0x1BCF, 0x28C4)  # vid, pid
+ARDUCAM = (0x0C45, 0x636D)
 
-def find_camera(
-    vid: int = 0x0C45,
-    pid: int = 0x636D,
+
+def find_camera(apiPreference: int = cv2.CAP_ANY) -> cv2.VideoCapture | None:
+    """Find and return the Reachy Mini camera.
+
+    Args:
+        apiPreference (int): Preferred API backend for the camera. Default is cv2.CAP_ANY.
+
+    Returns:
+        cv2.VideoCapture | None: A VideoCapture object if the camera is found and opened successfully, otherwise None.
+
+    """
+    cap = find_camera_by_vid_pid(RPICAM[0], RPICAM[1], apiPreference)
+    if cap is not None:
+        fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+        return cap
+
+    cap = find_camera_by_vid_pid(ARDUCAM[0], ARDUCAM[1], apiPreference)
+    if cap is not None:
+        return cap
+
+    return None
+
+
+def find_camera_by_vid_pid(
+    vid: int = RPICAM[0],
+    pid: int = RPICAM[1],
     apiPreference: int = cv2.CAP_ANY,
 ) -> cv2.VideoCapture | None:
     """Find and return a camera with the specified VID and PID.
