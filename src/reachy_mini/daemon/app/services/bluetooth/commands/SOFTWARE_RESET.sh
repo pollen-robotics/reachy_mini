fa40c1ada422c1fbb2090762f05f3af036effb69
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-11-04 11:21:55 +0100

    Update software reset to new tree.

--- src/reachy_mini/daemon/app/services/bluetooth/commands/SOFTWARE_RESET.sh
+++ src/reachy_mini/daemon/app/services/bluetooth/commands/SOFTWARE_RESET.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf /venvs/mini_daemon/
-cp -r /restore/mini_daemon /venvs/
-chown -R pollen:pollen /venvs/mini_daemon/
+rm -rf /venvs/
+cp -r /restore/ /venvs/
+chown -R pollen:pollen /venvs
 systemctl restart reachy-mini-daemon.service
 
