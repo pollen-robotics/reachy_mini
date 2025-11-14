cce8dfac5611c1cfc96185a73cbbe63d11feffa9
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-11-14 10:01:27 +0100

    Update shutdown_monitor.py

--- src/reachy_mini/daemon/app/services/gpio_shutdown/shutdown_monitor.py
+++ src/reachy_mini/daemon/app/services/gpio_shutdown/shutdown_monitor.py
@@ -1,10 +1,11 @@
 """Monitor GPIO24 for shutdown signal."""
 
 from signal import pause
+from subprocess import call
 
 from gpiozero import Button
 
-shutdown_button = Button(24)
+shutdown_button = Button(24, pull_up=False)
 
 # log_file = "/var/log/shutdown_button.log"
 log_file = "/vens/shutdown_button.log"
@@ -12,20 +13,11 @@ log_file = "/vens/shutdown_button.log"
 
 def released() -> None:
     """Handle shutdown button released."""
-    print("Shutdown button released! Shutting down...")
-    with open(log_file) as f:
-        f.write("Shutdown button released! Shutting down...\n")
-
-
-def pressed() -> None:
-    """Handle shutdown button pressed."""
-    print("Shutdown button pressed!")
-    with open(log_file) as f:
-        f.write("Shutdown button pressed!\n")
+    call(["sudo", "systemctl", "stop", "reachy-mini-daemon"])
+    call(["sudo", "shutdown", "-h", "now"])
 
 
 shutdown_button.when_released = released
-shutdown_button.when_pressed = pressed
 
 print("Monitoring GPIO24 for shutdown signal...")
 pause()
