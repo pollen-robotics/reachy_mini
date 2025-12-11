c8dad55ed50b32bf4b286eba687123b388953ba7
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2025-12-11 11:36:19 +0100

    using daemon for position reset

--- src/reachy_mini/daemon/app/main.py
+++ src/reachy_mini/daemon/app/main.py
@@ -134,6 +134,7 @@ def create_app(args: Args, health_check_event: asyncio.Event | None = None) -> F
     app.state.app_manager = AppManager(
         wireless_version=args.wireless_version,
         desktop_app_daemon=args.desktop_app_daemon,
+        daemon=app.state.daemon,
     )
 
     router = APIRouter(prefix="/api")
