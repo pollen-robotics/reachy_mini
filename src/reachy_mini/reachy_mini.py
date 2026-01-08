bd0b2ac877f7c0f228ac700d8defc56f61dfb101
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2026-01-08 17:36:54 +0100

    enhancement #572: using gstreamer backend on lite

--- src/reachy_mini/reachy_mini.py
+++ src/reachy_mini/reachy_mini.py
@@ -214,32 +214,15 @@ class ReachyMini:
                 # Lite version: use specified backend if compatible
                 try:
                     mbackend = MediaBackend(media_backend.lower())
-                    if mbackend == MediaBackend.WEBRTC:
-                        self.logger.warning(
-                            f"Incompatible media backend on Lite: {media_backend}, using default backend."
-                        )
-                        mbackend = MediaBackend.DEFAULT
-                    # TODO : Remove when wheel is released !
-                    elif "gstreamer" in media_backend.lower() and (
-                        platform.system() == "Darwin" or platform.system() == "Windows"
-                    ):
-                        self.logger.warning(
-                            f"Unsupported media backend on Lite for {platform.system()}: {media_backend}, using default backend."
-                        )
-                        mbackend = (
-                            MediaBackend.DEFAULT_NO_VIDEO
-                            if "no_video" in media_backend.lower()
-                            else MediaBackend.DEFAULT
-                        )
-                    else:
-                        self.logger.info(
-                            f"Auto-detected: Lite. Using {mbackend} backend."
-                        )
                 except ValueError:
                     self.logger.warning(
                         f"Invalid media backend on Lite: {media_backend}, using default backend."
                     )
-                    mbackend = MediaBackend.DEFAULT
+                    mbackend = (
+                            MediaBackend.DEFAULT_NO_VIDEO
+                            if "no_video" in media_backend.lower()
+                            else MediaBackend.DEFAULT
+                    )
 
         return MediaManager(
             use_sim=self.client.get_status()["simulation_enabled"],
