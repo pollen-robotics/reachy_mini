7e4d2abfed5c74a07570f91b9c9934539ad2106a
Pierre Rouanet <pierre.rouanet@gmail.com>
2026-01-19 11:22:16 +0100

    Update reachy_mini.py

--- src/reachy_mini/reachy_mini.py
+++ src/reachy_mini/reachy_mini.py
@@ -536,8 +536,8 @@ class ReachyMini:
         points = np.array([[[u, v]]], dtype=np.float32)
         x_n, y_n = cv2.undistortPoints(
             points,
-            self.media.camera.K,  # type: ignore
-            self.media.camera.D,  # type: ignore
+            self.media.camera.K,
+            self.media.camera.D,
         )[0, 0]
 
         ray_cam = np.array([x_n, y_n, 1.0])
