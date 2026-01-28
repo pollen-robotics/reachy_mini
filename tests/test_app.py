5704bd4c3b5e91c580cec0f54084004e03cce294
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2026-01-28 12:17:29 +0100

    enhancement #572: fix app unit test hanging

--- tests/test_app.py
+++ tests/test_app.py
@@ -46,38 +46,40 @@ async def test_app_manager() -> None:
     )
 
     app_mngr = AppManager()
+    try:
+        before_installed_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
 
-    before_installed_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
+        app_info = AppInfo(
+            name="ok_app",
+            source_kind=SourceKind.LOCAL,
+            extra={"path": str(Path(__file__).parent / "ok_app")},
+        )
+        await app_mngr.install_new_app(app_info, daemon.logger)
 
-    app_info = AppInfo(
-        name="ok_app",
-        source_kind=SourceKind.LOCAL,
-        extra={"path": str(Path(__file__).parent / "ok_app")},
-    )
-    await app_mngr.install_new_app(app_info, daemon.logger)
+        after_installed_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
 
-    after_installed_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
+        assert len(after_installed_apps) == len(before_installed_apps) + 1
 
-    assert len(after_installed_apps) == len(before_installed_apps) + 1
+        status = await app_mngr.start_app("ok_app", media_backend="no_media")
+        assert status is not None and status.state in (AppState.STARTING, AppState.RUNNING)
+        assert app_mngr.is_app_running()
+        status = await app_mngr.current_app_status()
+        assert status is not None and status.state in (AppState.STARTING, AppState.RUNNING)
 
-    status = await app_mngr.start_app("ok_app", media_backend="no_media")
-    assert status is not None and status.state in (AppState.STARTING, AppState.RUNNING)
-    assert app_mngr.is_app_running()
-    status = await app_mngr.current_app_status()
-    assert status is not None and status.state in (AppState.STARTING, AppState.RUNNING)
+        await app_mngr.stop_current_app()
+        assert not app_mngr.is_app_running()
+        status = await app_mngr.current_app_status()
+        assert status is None
 
-    await app_mngr.stop_current_app()
-    assert not app_mngr.is_app_running()
-    status = await app_mngr.current_app_status()
-    assert status is None
+        await app_mngr.remove_app("ok_app", daemon.logger)
+        after_uninstalled_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
 
-    await app_mngr.remove_app("ok_app", daemon.logger)
-    after_uninstalled_apps = await app_mngr.list_available_apps(SourceKind.INSTALLED)
+        assert len(after_uninstalled_apps) == len(before_installed_apps)
 
-    assert len(after_uninstalled_apps) == len(before_installed_apps)
-
-    await daemon.stop(goto_sleep_on_stop=False)
-
+    except Exception as e:
+        pytest.fail(f"install_new_app raised an exception: {e}")
+    finally:
+        await daemon.stop(goto_sleep_on_stop=False)
 
 
 @pytest.mark.asyncio
@@ -97,23 +99,28 @@ async def test_faulty_app() -> None:
         source_kind=SourceKind.LOCAL,
         extra={"path": str(Path(__file__).parent / "faulty_app")},
     )
-    await app_mngr.install_new_app(app_info, daemon.logger)
+    try:
+        await app_mngr.install_new_app(app_info, daemon.logger)
 
-    status = await app_mngr.start_app("faulty_app", media_backend="no_media")
+        status = await app_mngr.start_app("faulty_app", media_backend="no_media")
 
-    success = False
-    for _ in range(10):
-        status = await app_mngr.current_app_status()
-        if status is None or status.state in (AppState.STARTING, AppState.RUNNING):
-            await asyncio.sleep(1.0)
-            continue
+        success = False
+        for _ in range(10):
+            status = await app_mngr.current_app_status()
+            if status is None or status.state in (AppState.STARTING, AppState.RUNNING):
+                await asyncio.sleep(1.0)
+                continue
 
-        if status is not None and status.state == AppState.ERROR:
-            success = True
-            break
+            if status is not None and status.state == AppState.ERROR:
+                success = True
+                break
 
-    await app_mngr.remove_app("faulty_app", daemon.logger)
-    await daemon.stop(goto_sleep_on_stop=False)
+        await app_mngr.remove_app("faulty_app", daemon.logger)
+  
+        if not success:
+            pytest.fail("Faulty app did not reach ERROR state in time")
 
-    if not success:
-        pytest.fail("Faulty app did not reach ERROR state in time")
+    except Exception as e:
+        pytest.fail(f"install_new_app raised an exception: {e}")
+    finally:
+        await daemon.stop(goto_sleep_on_stop=False)
