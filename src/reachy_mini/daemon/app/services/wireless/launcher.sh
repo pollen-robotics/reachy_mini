c9549bb1fc7828aefb90d0703492d39443b4317b
Fabien <FabienDanieau@users.noreply.github.com>
2026-05-27 16:18:26 +0200

    add path to the new libcamera and gstreamer libs (#1149)

--- src/reachy_mini/daemon/app/services/wireless/launcher.sh
+++ src/reachy_mini/daemon/app/services/wireless/launcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 source /venvs/mini_daemon/bin/activate
-export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:/opt/gst-plugins-rs/lib/aarch64-linux-gnu/
+export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:/opt/gst-plugins-rs/lib/aarch64-linux-gnu/:/usr/local/lib/aarch64-linux-gnu/gstreamer-1.0/
 export PATH=$PATH:/opt/uv
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/aarch64-linux-gnu/
+export LIBCAMERA_IPA_MODULE_PATH=/usr/local/lib/aarch64-linux-gnu/libcamera/ipa
+export LIBCAMERA_IPA_CONFIG_PATH=/usr/local/share/libcamera/ipa
 
 # Ensure WiFi is not soft-blocked (can happen after a crash or kernel module reload)
 sudo rfkill unblock wifi
