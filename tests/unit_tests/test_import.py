5446ebd54fd93a0147961748fa4dc68d9297a5b6
Alina Lozowski <ailozowski@gmail.com>
2026-08-12 14:07:10 +0200

    test: simplify deferred backend import coverage

--- tests/unit_tests/test_import.py
+++ tests/unit_tests/test_import.py
@@ -12,14 +12,10 @@ def test_import_defers_daemon_backend() -> None:
         import sys
 
         import reachy_mini
-
-        assert "reachy_mini.daemon.backend.abstract" not in sys.modules
-
         from reachy_mini import ReachyMini, ReachyMiniApp
         from reachy_mini.io import WSServer
-        from reachy_mini.io.ws_server import WSServer as DirectWSServer
 
-        assert WSServer is DirectWSServer
+        assert "reachy_mini.daemon.backend.abstract" not in sys.modules
         """
     )
 
