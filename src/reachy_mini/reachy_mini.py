ef6e97afe47d5654991f4009f507480f735fa97c
RemiFabre <remifabre1800@gmail.com>
2026-09-11 11:42:55 +0200

    Drop formatting-only changes to reachy_mini.py

--- src/reachy_mini/reachy_mini.py
+++ src/reachy_mini/reachy_mini.py
@@ -64,10 +64,7 @@ SLEEP_HEAD_JOINT_POSITIONS = [
 ]
 
 
-INIT_ANTENNAS_JOINT_POSITIONS = [
-    -0.1745,
-    0.1745,
-]  # ~10° offset to reduce shaking at vertical
+INIT_ANTENNAS_JOINT_POSITIONS = [-0.1745, 0.1745]  # ~10° offset to reduce shaking at vertical
 SLEEP_ANTENNAS_JOINT_POSITIONS = [-3.05, 3.05]
 SLEEP_HEAD_POSE = np.array(
     [
@@ -238,9 +235,7 @@ class ReachyMini:
             return
 
         self.media_manager.close()
-        self.media_manager = self._configure_mediamanager(
-            self._media_backend, self._log_level
-        )
+        self.media_manager = self._configure_mediamanager(self._media_backend, self._log_level)
         self._media_released = False
         self.logger.info("Media re-acquired by daemon.")
 
@@ -258,10 +253,7 @@ class ReachyMini:
         incoming WebRTC audio also produce head movement.
 
         """
-
-        def _send_offsets(
-            offsets: tuple[float, float, float, float, float, float],
-        ) -> None:
+        def _send_offsets(offsets: tuple[float, float, float, float, float, float]) -> None:
             try:
                 self.client.send_command(SetSpeechOffsetsCmd(offsets=list(offsets)))
             except ConnectionError:
@@ -731,14 +723,13 @@ class ReachyMini:
 
     def wake_up(self) -> None:
         """Wake up the robot - go to the initial head position and play the wake up emote and sound."""
-        self.goto_target(
-            INIT_HEAD_POSE, antennas=INIT_ANTENNAS_JOINT_POSITIONS, duration=2
-        )
+        self.goto_target(INIT_HEAD_POSE, antennas=INIT_ANTENNAS_JOINT_POSITIONS, duration=2)
         time.sleep(0.1)
 
         # Toudoum
         self.media.play_sound("wake_up.wav")
 
+        # Roll 20° to the left
         pose = INIT_HEAD_POSE.copy()
         pose[:3, :3] = R.from_euler("xyz", [20, 0, 0], degrees=True).as_matrix()
         self.goto_target(pose, duration=0.2)
@@ -764,9 +755,7 @@ class ReachyMini:
         ]
         dist = np.linalg.norm(np.array(current_positions) - np.array(init_positions))
         if dist > 0.2:
-            self.goto_target(
-                INIT_HEAD_POSE, antennas=INIT_ANTENNAS_JOINT_POSITIONS, duration=1
-            )
+            self.goto_target(INIT_HEAD_POSE, antennas=INIT_ANTENNAS_JOINT_POSITIONS, duration=1)
             time.sleep(0.2)
 
         # Pfiou
